827c67dec99598f6e9742b0f84f57aef6e383209
wangyukun <6475593@qq.com>
2016-03-26 00:33:33 +0800

    Initial Commit

---
+++ 百思不得姐/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  百思不得姐
+//
+//  Created by 王玉琨 on 16/3/26.
+//  Copyright © 2016年 王玉琨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 百思不得姐/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  百思不得姐
+//
+//  Created by 王玉琨 on 16/3/26.
+//  Copyright © 2016年 王玉琨. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
