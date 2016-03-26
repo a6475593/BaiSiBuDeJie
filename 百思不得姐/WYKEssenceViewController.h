d28bdaf81816f491a5cbef3a7db090d4819b8f65
wangyukun <6475593@qq.com>
2016-03-26 21:58:50 +0800

    123

---
+++ 百思不得姐/WYKEssenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  WYKEssenceViewController.h
+//  百思不得姐
+//
+//  Created by 王玉琨 on 16/3/26.
+//  Copyright © 2016年 王玉琨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYKEssenceViewController : UIViewController
+
+@end
--- 百思不得姐/WYKTabBarController.m
+++ 百思不得姐/WYKTabBarController.m
@@ -7,7 +7,10 @@
 //
 
 #import "WYKTabBarController.h"
-
+#import "WYKEssenceViewController.h"
+#import "WYKNewViewController.h"
+#import "WYKFriendTrendsViewController.h"
+#import "WYKMeViewController.h"
 @interface WYKTabBarController ()
 
 @end
@@ -17,52 +20,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
     NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
     attrs[NSFontAttributeName] = [UIFont systemFontOfSize:11];
     attrs[NSForegroundColorAttributeName] = [UIColor lightGrayColor];
     
     NSMutableDictionary *selectedattrs = [NSMutableDictionary dictionary];
-    selectedattrs[NSFontAttributeName] = [UIFont systemFontOfSize:11];
+    selectedattrs[NSFontAttributeName] = attrs[NSFontAttributeName];
     selectedattrs[NSForegroundColorAttributeName] = [UIColor grayColor];
     
-    UIViewController *vc01 = [[UIViewController alloc] init];
-    vc01.tabBarItem.title = @"精华";
-    vc01.tabBarItem.image = [UIImage imageNamed:@"tabBar_essence_icon"];
-    vc01.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_essence_click_icon"];
-    vc01.view.backgroundColor = [UIColor redColor];
-    [vc01.tabBarItem setTitleTextAttributes:attrs forState:UIControlStateNormal];
-    [vc01.tabBarItem setTitleTextAttributes:selectedattrs forState:UIControlStateSelected];
-    [self addChildViewController:vc01];
-    UIViewController *vc02 = [[UIViewController alloc] init];
-    vc02.tabBarItem.title = @"新帖";
-    vc02.tabBarItem.image = [UIImage imageNamed:@"tabBar_new_icon"];
-    vc02.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_new_click_icon"];
-    vc02.view.backgroundColor = [UIColor redColor];
-    [vc02.tabBarItem setTitleTextAttributes:attrs forState:UIControlStateNormal];
-    [vc02.tabBarItem setTitleTextAttributes:selectedattrs forState:UIControlStateSelected];
-    [self addChildViewController:vc02];
-    
-    UIViewController *vc03 = [[UIViewController alloc] init];
-    vc03.tabBarItem.title = @"关注";
-    vc03.tabBarItem.image = [UIImage imageNamed:@"tabBar_friendTrends_icon"];
-    vc03.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_friendTrends_click_icon"];
-    vc03.view.backgroundColor = [UIColor redColor];
-    [vc03.tabBarItem setTitleTextAttributes:attrs forState:UIControlStateNormal];
-    [vc03.tabBarItem setTitleTextAttributes:selectedattrs forState:UIControlStateSelected];
-    [self addChildViewController:vc03];
-    
-    UIViewController *vc04 = [[UIViewController alloc] init];
-    vc04.tabBarItem.title = @"我的";
-    vc04.tabBarItem.image = [UIImage imageNamed:@"tabBar_me_icon"];
-    vc04.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_me_click_icon"];
-    vc04.view.backgroundColor = [UIColor redColor];
-    [vc04.tabBarItem setTitleTextAttributes:attrs forState:UIControlStateNormal];
-    [vc04.tabBarItem setTitleTextAttributes:selectedattrs forState:UIControlStateSelected];
-    [self addChildViewController:vc04];
+    UITabBarItem *item = [UITabBarItem appearance];
+    [item setTitleTextAttributes:attrs forState:UIControlStateNormal];
+    [item setTitleTextAttributes:selectedattrs forState:UIControlStateSelected];
     
     
-}
+    [self setupChildVc:[[WYKEssenceViewController alloc] init] title:@"精华" image:@"tabBar_essence_icon" selectedImage:@"tabBar_essence_click_icon"];
+    [self setupChildVc:[[WYKNewViewController alloc] init] title:@"新帖" image:@"tabBar_new_icon" selectedImage:@"tabBar_new_click_icon"];
+    [self setupChildVc:[[WYKFriendTrendsViewController alloc] init] title:@"关注" image:@"tabBar_friendTrends_icon" selectedImage:@"tabBar_friendTrends_click_icon"];
+    [self setupChildVc:[[WYKMeViewController alloc] init] title:@"我的" image:@"tabBar_me_icon" selectedImage:@"tabBar_me_click_icon"];
 
+
+}
+- (void)setupChildVc:(UIViewController *)vc title:(NSString *)title image:(NSString *)image selectedImage:(NSString *)selectedImage{
+    vc.tabBarItem.title = title;
+    vc.tabBarItem.image = [UIImage imageNamed:image];
+    vc.tabBarItem.selectedImage = [UIImage imageNamed:selectedImage];
+    vc.view.backgroundColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:1];
+    [self addChildViewController:vc];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
